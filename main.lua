c827c3cbb6adec47a5777ae7ee46aa6f04cd2968
Spectre <antoine@doussaud.org>
2019-05-07 18:29:31 +0200

    init

--- main.lua
+++ main.lua
@@ -1,15 +1,19 @@
 function love.load(arg)
-	print(arg[1])
-	local img = love.image.newImageData(arg[1])
-	print(string.format("data[%d * %d] = {", img:getWidth(), img:getHeight()))
-	for y=0,img:getHeight()-1 do
-		io.write("\t")
-		for x=0,img:getWidth()-1 do
-			local r,g,b = img:getPixel(x, y)
-			io.write(string.format("0x%02x%02x%02x, ", r*255, g*255, b*255))
+	if arg[1] then
+		print(arg[1])
+		local img = love.image.newImageData(arg[1])
+		print(string.format("data[%d * %d] = {", img:getWidth(), img:getHeight()))
+		for y=0,img:getHeight()-1 do
+			io.write("\t")
+			for x=0,img:getWidth()-1 do
+				local r,g,b = img:getPixel(x, y)
+				io.write(string.format("0x%02x%02x%02x, ", r*255, g*255, b*255))
+			end
+			print()
 		end
-		print()
+		print("}")
+	else
+		print("No file")
 	end
-	print("}")
 	love.event.quit()
 end
